04bdd9a688e01cb44fb531a4e55c5ae441661916
Rémi Guillette <remiguillette@gmail.com>
2026-01-31 17:45:25 -0500

    Add Android-style status bar clock

--- src/components/BeaverDashboard.tsx
+++ src/components/BeaverDashboard.tsx
@@ -5,10 +5,19 @@ import { WeatherWidget } from "./widgets/WeatherWidget";
 export function BeaverDashboard() {
   return (
     <div className="dashboard">
+      <header className="android-status-bar">
+        <div className="status-bar__left">
+          <ClockWidget />
+        </div>
+        <div className="status-bar__right">
+          <span className="status-icon">📶 5G</span>
+          <span className="status-icon">🛜</span>
+          <span className="status-icon">🔋 85%</span>
+        </div>
+      </header>
       <section className="dashboard__grid">
         <DeezerWidget />
         <WeatherWidget />
-        <ClockWidget />
       </section>
     </div>
   );
