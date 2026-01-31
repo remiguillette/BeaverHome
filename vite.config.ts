e9e64fb7a15ea7d434fc7f8c0a22764763a19e92
Rémi Guillette <remiguillette@gmail.com>
2026-01-31 15:36:43 -0500

    Fix proxy target in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5000,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5001",
+        target: "http://127.0.0.1:5000",
         changeOrigin: true
       }
     }
